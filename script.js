c2d0a69ad5fd13c051c2752fae0429c6dcc337a2
SemiTone <alexeyton@mail.ru>
2023-11-18 15:30:36 +0500

    was optimize mobile content

--- script.js
+++ script.js
@@ -5,50 +5,7 @@ const btns = document.querySelectorAll('.btn')
 btns.forEach((btn) => btn.addEventListener('click', function() {
     
     nav.classList.toggle('nav__list-large')
-    // nav.classList.toggle('nav__list')
-    
-   
-    
+    // nav.classList.toggle('nav__list') 
 }))
-
-
-
-
-// if (btn) {
-//     console.log('est')
-//     btn.addEventListener('click', function() {
-//         nav.classList.toggle('nav__list')
-//         nav.classList.toggle('nav__list-large')
-        
-        
-//     })
-// }
-
-
-
-// const navButton = document.querySelector('.header__nav-btn-large');
-// const navButtonOff = document.querySelector('.header__nav-btn')
-
-// function openMenu() {
-//     const nav = document.querySelector('.nav__list');
-//     nav.style.width = '300px'
-    
-//     navButton.style.display = 'none'
-// }
-
-// function closeMenu() {
-//     const nav = document.querySelector('.nav__list');
-//     nav.style.display = 'none'
-//     navButton.style.display = 'block'
-// }
-
-
-// if (navButton) {
-//     navButton.addEventListener('click', openMenu)
-// }
-
-// if (navButtonOff) {
-//     navButtonOff.addEventListener('click', closeMenu)
-// }
 
 
